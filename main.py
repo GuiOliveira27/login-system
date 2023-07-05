ad15d5789d277afd85fcc8252eeddb3d93ffdaeb
GuiOliveira27 <guilherme_oliveira11@estudante.ifms.edu.br>
2023-07-05 18:34:11 -0400

    :feat: Development of a login system

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/contacts")
+def contacts():
+    return render_template("contacts.html")
+
+@app.route("/users/<user_name>")
+def users(user_name):
+    return render_template("users.html", user_name=user_name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
